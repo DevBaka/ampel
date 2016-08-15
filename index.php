65f9348bec65e7ef8bf2e1625b957441d53e493f
DevBaka <devbaka@gmx.net>
2016-08-15 14:59:07 +0200

    uodate

--- index.php
+++ index.php
@@ -7,28 +7,26 @@
 <body>
 <section>
 <h1> Ampel</h1>
-<div id="ampel1">
-	<div id="red1">
+<div id="ampel1" class="Ampel1">
+	<div id="red1" >
 	</div>
-	<div id="yellow1">
+	<div id="yellow1" >
 	</div>
-	<div id="green1">
+	<div id="green1" >
 	</div>
 </div>
 <form method="POST">
-	<input type="submit" name="enter" value="Pressed">
+	<input type="submit" name="enter" value="Pressed" id="btn" class="Ampel1">
 </form>
 <?php
 	require_once("sql.php");
 
-	$db = new DB();
-	echo $db->red1();
+	$_db = new MyDB();
+	echo $_db->baka();
 
 
 	if($_POST['enter']){
-		echo "hello world";
-		
-		
+		echo "hello world";		
 	}
 
 
